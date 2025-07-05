81f34208a5fef1da36924c215826e2671ba9dab9
Mireau-Jules <saintlouismireaujules@gmail.com>
2025-07-05 10:32:51 -0400

    Complete Lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,9 +1,16 @@
 import React from "react";
-function Article({ title, date = "January 1, 1970", preview }) {
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  const emoji = minutes < 30 ? "☕️" : "🍱";
+  const units = minutes < 30 ? Math.ceil(minutes / 5) : Math.ceil(minutes / 10);
+  const emojiString = emoji.repeat(units);
+
   return (
     <article>
       <h3>{title}</h3>
-      <small>{date}</small>
+      <small>
+        {date} • {emojiString} {minutes} min read
+      </small>
       <p>{preview}</p>
     </article>
   );
